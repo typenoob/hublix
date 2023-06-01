726b1eb2bb399b1ba6427b576b2210457ea12df3
typenoob <61347081+typenoob@users.noreply.github.com>
2023-06-01 09:46:49 +0800

    Update UserAvatar.vue

--- src/components/UserAvatar.vue
+++ src/components/UserAvatar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-avatar size="40" :color="name ? uniqolor(name[0]).color : 'white'">
-    <img :src="`http://127.0.0.1:7001${path}`" v-if="path" />
+    <img :src="`http://egg.web-framework-ao2f.1627164863551065.cn-hangzhou.fc.devsapp.net${path}`" v-if="path" />
     <span v-else class="white--text text-h5">{{
       name ? name[0] : "未知"
     }}</span>
